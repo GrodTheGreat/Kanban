4e8de4ad3ba6f6695ad1028ec1a4013195b8eca7
Gerard Causse <grod.causse@gmail.com>
2025-05-11 09:28:48 -0400

    Add shadows to cards

--- kanban-nuxt/components/Card.vue
+++ kanban-nuxt/components/Card.vue
@@ -12,9 +12,11 @@
 
 <style scoped>
 .card {
-  border-radius: 1rem;
+  border-radius: 0.25rem;
   background-color: whitesmoke;
   padding: 0.5rem;
+  min-height: 2rem;
+  box-shadow: 0 2px 4px black;
 }
 .card__actions {
   display: flex;
